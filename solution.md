588b56605b571982363f7355a5b385d876698efa
Clement Hamada <clementhamada@protonmail.com>
2021-11-23 21:00:27 +0100

    Initial commit

---
+++ solution.md
@@ -0,0 +1,12 @@
+```
+Nmap scan report for 192.168.56.1
+Host is up (0.00014s latency).
+Not shown: 996 closed tcp ports (conn-refused)
+PORT     STATE SERVICE
+22/tcp   open  ssh
+88/tcp   open  kerberos-sec
+3283/tcp open  netassistant
+5900/tcp open  vnc
+
+Nmap done: 1 IP address (1 host up) scanned in 51.89 seconds
+```
---
+++ vm.sh
@@ -0,0 +1,206 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+host_ssh_port=22
+
+iso_url="https://projects.intra.42.fr/uploads/document/document/2832/BornToSecHackMe-v1.1.iso"
+
+vm_dir="$PWD"
+
+vm_os="Linux_64"
+vm_image="$vm_dir/$(basename "$iso_url")"
+vm_name="Boot2Root"
+vm_ram="1024"
+vm_vram="0"
+vm_gfx="none"
+vm_net="hostonly"
+vm_bridge_adapter="en0"
+vm_hostonly_adapter="vboxnet0"
+
+vm_ssh_port=22
+
+print_help()
+{
+	echo -e "Usage $0 [command]
+Commands:
+    up      Setup and stat the virtual machine.
+    down    Unregister and delete the virtual machine.
+    ip      Show a running virtual machine's IPV4 address.
+    ssh     Connect to the virtual machine using SSH.
+    help    Show this help message.
+
+If no argument is provided, 'up' will be assumed."
+}
+
+print_ssh_usage()
+{
+	echo -e "Usage: $0 ssh user[:pass]"
+}
+
+print_vm_stopped()
+{
+	echo -e "'$vm_name' is not running!
+
+Use '$0 up' to start it up." 1>&2
+}
+
+print_vm_started()
+{
+	echo -e "'$vm_name' was already started!
+
+Use '$0 ssh' to connect."
+}
+
+vm_exists()
+{
+	VBoxManage showvminfo "$vm_name" > /dev/null 2>&1
+}
+
+vm_running()
+{
+	VBoxManage list runningvms | grep "\"$vm_name\"" > /dev/null
+}
+
+vm_checkif()
+{
+	local ifname="$1"
+
+	echo "Checking for interface $ifname..."
+
+	ifconfig "$ifname"
+}
+
+vm_net_hostonly_up()
+{
+	local ifname="$vm_hostonly_adapter"
+
+	vm_checkif "$ifname" || VBoxManage hostonlyif create
+	VBoxManage modifyvm "$vm_name" --hostonlyadapter1 "$ifname"
+}
+
+vm_net_bridged_up()
+{
+	VBoxManage modifyvm "$vm_name" --bridgeadapter1 "$vm_bridge_adapter"
+}
+
+vm_net_nat_up()
+{
+	VBoxManage modifyvm "$vm_name" --natpf1 "ssh,tcp,,$host_ssh_port,,$vm_ssh_port"
+}
+
+vm_up()
+{
+	if ! vm_exists
+	then
+		# Download the disk image.
+		pushd "$(dirname "$vm_image")"
+			while ! md5sum --check md5sums
+			do
+				echo "Fetching '$vm_image'..."
+
+				curl -L -O -C - "$iso_url" --output "$vm_image"
+			done
+		popd
+
+		echo "Initializing '$vm_name' at '$vm_dir'..."
+
+		# Create and register vm in current working directory.
+		VBoxManage createvm --name "$vm_name" --ostype "$vm_os" --register --basefolder "$vm_dir"
+		VBoxManage modifyvm "$vm_name" --memory "$vm_ram" --vram "$vm_vram" --graphicscontroller "$vm_gfx" --nic1 "$vm_net"
+	
+		case "$vm_net" in
+			"hostonly"	)	vm_net_hostonly_up;;
+			"bridged"	)	vm_net_bridged_up;;
+			"nat"		)	vm_net_nat_up;;
+		esac
+
+		# Add an IDE controller
+		VBoxManage storagectl "$vm_name" --name "IDE Controller" --add ide --controller PIIX4
+
+		# Attach the disk image.
+		VBoxManage storageattach "$vm_name" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "$vm_image"
+	fi
+
+	if ! vm_running
+	then
+		echo "Starting '$vm_name'..."
+		VBoxManage startvm "$vm_name" --type headless
+	else
+		print_vm_started 2>&1
+		return 1
+	fi
+}
+
+vm_down()
+{
+	if vm_exists
+	then
+		if vm_running
+		then
+			echo "Waiting for '$vm_name' to power off,,,"
+			VBoxManage controlvm "$vm_name" poweroff && sleep 2
+		fi
+		echo "Tearing down '$vm_name'..."
+		VBoxManage unregistervm "$vm_name" --delete
+	fi
+}
+
+vm_ipv4()
+{
+	if vm_running
+	then
+		case "$vm_net" in
+			"hostonly"	)	ifconfig vboxnet0 | egrep "inet\s*([0-9\.])*" -o | cut -d ' ' -f 2;;
+			"bridged"	)	VBoxManage guestproperty get "$vm_name" /VirtualBox/GuestInfo/Net/0/V4/IP | cut -d' ' -f2;;
+			"nat"		)	echo "localhost";;
+			"*"			)	echo "The '$vm_net' network mode is not supported!"
+		esac
+	else
+		print_vm_stopped 2>&1
+		return 1
+	fi
+}
+
+vm_ssh() # user
+{
+	local user="${1:-}"
+
+	if [ -z "$user" ]
+	then
+		echo "$0: ssh: Missing user argument." 2>&1
+		print_ssh_usage 2>&1
+		return 1
+	fi
+
+	if vm_running
+	then
+		if [ "$vm_net" = nat ]
+		then
+			local port="$host_ssh_port"
+		else
+			local port="$vm_ssh_port"
+		fi
+
+		local pass="${user##*:}"
+
+		if ! [ -z "$pass" ]
+		then
+			"$(dirname "$0")/utils/pass.exp" "$pass" ssh -p "$port" "${user%%:*}@$(vm_ipv4)"
+		else
+			ssh -p "$port" "$user@$(vm_ipv4)"
+		fi
+	else
+		print_vm_stopped 2>&1
+		return 1
+	fi
+}
+
+case "${1:-}" in
+	"up" | ""	)	vm_up;;
+	"down"		)	vm_down;;
+	"ip"		)	vm_ipv4;;
+	"ssh"		)	shift; vm_ssh $@;;
+	"help"		)	print_help;;
+	*			)	print_help && exit 1;;
+esac
